a79535988716d1de22479cbf40b406081e61b0e6
Kassandra <alinka.makarova.2000@bk.ru>
2025-10-09 12:00:16 +0300

    Текущие изменения

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        DoublyLinkedCyclicList<Integer> list = new DoublyLinkedCyclicList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
 
+        System.out.println(list);
+        list.addByIndex(567, 1);
+        System.out.println(list);
+        list.addFirst(172);
+        System.out.println(list);
+        if (list.contains(172)) {
+            System.out.println("172 найдено в списке");
+        }
+        System.out.println("Элементов в списке: " + list.size());
+        list.deleteNode(561);
+        System.out.println(list);
+        list.deleteByIndex(0);
+        System.out.println(list);
+        System.out.println(list.get(2));
+
+        list.clear();
+        System.out.println(list);
     }
 }
